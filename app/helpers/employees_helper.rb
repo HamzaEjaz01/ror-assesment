59e5b7581c1bbbb18ee4106b76708ec297f12313
HamzaEjaz01 <hamzaejaz97@gmail.com>
2024-03-05 22:07:31 +0500

    Refactor views

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,2 +1,8 @@
 module EmployeesHelper
+  def render_form_field(label, attribute, value = nil, type: :text)
+    content_tag :div, class: 'mb-3' do
+      concat label_tag attribute, label, class: 'form-label'
+      concat send("#{type}_field_tag", attribute, value, class: 'form-control')
+    end
+  end
 end
